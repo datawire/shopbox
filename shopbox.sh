d2f424c93991debb2e88b29517dcd1149833c513
Richard Li <richard@datawire.io>
2017-09-21 11:15:48 -0400

    create a workdir

--- shopbox.sh
+++ shopbox.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-docker run --rm -it \
+mkdir -p workspace
+cd workspace
+
+sudo docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):/workspace \
     datawire/shopbox:latest
-    
