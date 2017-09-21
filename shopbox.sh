6eb2edd359fc546ca49c2b065b73615b175b8aea
Philip Lombardi <plombardi@datawire.io>
2017-09-21 11:59:21 -0400

    dont use workspace directory

--- shopbox.sh
+++ shopbox.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-mkdir -p workspace
-cd workspace
-
 sudo docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):/workspace \
