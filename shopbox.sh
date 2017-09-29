fff1e3763f26f9f2d27be1debfd53d03a286fc38
Philip Lombardi <plombardi@datawire.io>
2017-09-29 16:04:58 -0400

    make shopbox easier to use with minikube

--- shopbox.sh
+++ shopbox.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${HOME}/.kube:/home/shopbox/.kube \
+    -v ${HOME}/.minikube:${HOME}/.minikube \
     -v $(pwd):/workspace \
     --privileged \
-    datawire/shopbox:latest \
+    datawire/shopbox:latest
