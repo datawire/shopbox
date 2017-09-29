eae4005f7dc0a2c709db0aeeb97c6f88d74742cc
Philip Lombardi <plombardi@datawire.io>
2017-09-29 16:32:27 -0400

    update shopbox script

--- shopbox.sh
+++ shopbox.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+docker pull datawire/shopbox:latest
+
 docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ${HOME}/.kube:/home/shopbox/.kube \
